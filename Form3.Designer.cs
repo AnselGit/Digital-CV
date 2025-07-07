d935064bc069090faaf5a8de2f5e74c5f0088af0
John Ansel Doton <johnanseldoton@gmail.com>
2025-07-07 16:01:22 +0800

    form3 structure

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -37,40 +37,64 @@
             this.lbl_name = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lkl_close = new System.Windows.Forms.LinkLabel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.flowLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
             this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
             this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(914, 857);
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(914, 1119);
             this.flowLayoutPanel1.TabIndex = 0;
             this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
             // 
             // panel1
             // 
+            this.panel1.AutoScroll = true;
             this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.lkl_close);
+            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.lbl_heading2);
             this.panel1.Controls.Add(this.lbl_heading3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.lbl_name);
             this.panel1.Controls.Add(this.lbl_heading);
+            this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(911, 854);
+            this.panel1.Size = new System.Drawing.Size(911, 1100);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // lbl_heading
             // 
             this.lbl_heading.BackColor = System.Drawing.Color.Transparent;
             this.lbl_heading.Font = new System.Drawing.Font("MODERNIZ", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_heading.ForeColor = System.Drawing.Color.White;
-            this.lbl_heading.Location = new System.Drawing.Point(34, 96);
+            this.lbl_heading.Location = new System.Drawing.Point(63, 71);
             this.lbl_heading.Name = "lbl_heading";
             this.lbl_heading.Size = new System.Drawing.Size(367, 56);
             this.lbl_heading.TabIndex = 0;
@@ -82,7 +106,7 @@
             this.lbl_heading2.BackColor = System.Drawing.Color.Transparent;
             this.lbl_heading2.Font = new System.Drawing.Font("MODERNIZ", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_heading2.ForeColor = System.Drawing.Color.White;
-            this.lbl_heading2.Location = new System.Drawing.Point(34, 152);
+            this.lbl_heading2.Location = new System.Drawing.Point(63, 127);
             this.lbl_heading2.Name = "lbl_heading2";
             this.lbl_heading2.Size = new System.Drawing.Size(367, 55);
             this.lbl_heading2.TabIndex = 1;
@@ -93,7 +117,7 @@
             this.lbl_heading3.BackColor = System.Drawing.Color.Transparent;
             this.lbl_heading3.Font = new System.Drawing.Font("MODERNIZ", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_heading3.ForeColor = System.Drawing.Color.White;
-            this.lbl_heading3.Location = new System.Drawing.Point(34, 207);
+            this.lbl_heading3.Location = new System.Drawing.Point(63, 182);
             this.lbl_heading3.Name = "lbl_heading3";
             this.lbl_heading3.Size = new System.Drawing.Size(100, 55);
             this.lbl_heading3.TabIndex = 2;
@@ -105,7 +129,7 @@
             this.lbl_name.BackColor = System.Drawing.Color.Transparent;
             this.lbl_name.Font = new System.Drawing.Font("Mongolian Baiti", 100F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_name.ForeColor = System.Drawing.Color.White;
-            this.lbl_name.Location = new System.Drawing.Point(70, 194);
+            this.lbl_name.Location = new System.Drawing.Point(99, 169);
             this.lbl_name.Name = "lbl_name";
             this.lbl_name.Size = new System.Drawing.Size(320, 127);
             this.lbl_name.TabIndex = 3;
@@ -117,7 +141,7 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 100F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(245, 321);
+            this.label1.Location = new System.Drawing.Point(251, 296);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(392, 127);
             this.label1.TabIndex = 4;
@@ -129,20 +153,144 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 100F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(468, 461);
+            this.label2.Location = new System.Drawing.Point(464, 417);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(399, 127);
             this.label2.TabIndex = 5;
             this.label2.Text = "Doton";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(468, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(369, 140);
+            this.label3.TabIndex = 6;
+            this.label3.Text = resources.GetString("label3.Text");
+            // 
+            // lkl_close
+            // 
+            this.lkl_close.AutoSize = true;
+            this.lkl_close.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lkl_close.LinkColor = System.Drawing.Color.White;
+            this.lkl_close.Location = new System.Drawing.Point(840, 11);
+            this.lkl_close.Name = "lkl_close";
+            this.lkl_close.Size = new System.Drawing.Size(58, 24);
+            this.lkl_close.TabIndex = 7;
+            this.lkl_close.TabStop = true;
+            this.lkl_close.Text = "Close";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Mongolian Baiti", 24F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(466, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 34);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Personal";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label5.Font = new System.Drawing.Font("Mongolian Baiti", 24F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(68, 427);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(175, 34);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Contact Me!";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(70, 472);
+            this.label6.Name = "label6";
+            this.label6.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label6.Size = new System.Drawing.Size(271, 60);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "📱: 0991 774 7938\r\n📧: johnanseldoton@gmail.com\r\n📍: Sta. Maria, Sto. Tomas, Bata" +
+    "ngas";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label7.Font = new System.Drawing.Font("Mongolian Baiti", 24F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(68, 544);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(159, 34);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Experience";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(70, 589);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(285, 60);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Production Operator (Working Student)\r\nEpson Precision Philippines Inc.\r\n🕒 Sept " +
+    "2021 – Jan 2022\r\n";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label9.Font = new System.Drawing.Font("Mongolian Baiti", 24F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(466, 565);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(145, 34);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Education";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(468, 610);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(328, 400);
+            this.label10.TabIndex = 13;
+            this.label10.Text = resources.GetString("label10.Text");
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(74, 630);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(380, 380);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(938, 881);
+            this.ClientSize = new System.Drawing.Size(938, 1100);
             this.ControlBox = false;
             this.Controls.Add(this.flowLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -152,6 +300,8 @@
             this.Load += new System.EventHandler(this.Form3_Load);
             this.flowLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -166,5 +316,15 @@
         private System.Windows.Forms.Label lbl_name;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel lkl_close;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -46,5 +46,25 @@ namespace DigitalCV
         {
 
         }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
