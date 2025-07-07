19acc1c08a02eb4f7a0e99907bd5049a669b48ef
John Ansel Doton <johnanseldoton@gmail.com>
2025-07-07 14:33:18 +0800

    form3 started

--- Form3.Designer.cs
+++ Form3.Designer.cs
@@ -28,12 +28,143 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_heading = new System.Windows.Forms.Label();
+            this.lbl_heading2 = new System.Windows.Forms.Label();
+            this.lbl_heading3 = new System.Windows.Forms.Label();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(914, 857);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.lbl_heading2);
+            this.panel1.Controls.Add(this.lbl_heading3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lbl_name);
+            this.panel1.Controls.Add(this.lbl_heading);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(911, 854);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbl_heading
+            // 
+            this.lbl_heading.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_heading.Font = new System.Drawing.Font("MODERNIZ", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_heading.ForeColor = System.Drawing.Color.White;
+            this.lbl_heading.Location = new System.Drawing.Point(34, 96);
+            this.lbl_heading.Name = "lbl_heading";
+            this.lbl_heading.Size = new System.Drawing.Size(367, 56);
+            this.lbl_heading.TabIndex = 0;
+            this.lbl_heading.Text = "Bonjour";
+            this.lbl_heading.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_heading2
+            // 
+            this.lbl_heading2.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_heading2.Font = new System.Drawing.Font("MODERNIZ", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_heading2.ForeColor = System.Drawing.Color.White;
+            this.lbl_heading2.Location = new System.Drawing.Point(34, 152);
+            this.lbl_heading2.Name = "lbl_heading2";
+            this.lbl_heading2.Size = new System.Drawing.Size(367, 55);
+            this.lbl_heading2.TabIndex = 1;
+            this.lbl_heading2.Text = "My Name";
+            // 
+            // lbl_heading3
+            // 
+            this.lbl_heading3.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_heading3.Font = new System.Drawing.Font("MODERNIZ", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_heading3.ForeColor = System.Drawing.Color.White;
+            this.lbl_heading3.Location = new System.Drawing.Point(34, 207);
+            this.lbl_heading3.Name = "lbl_heading3";
+            this.lbl_heading3.Size = new System.Drawing.Size(100, 55);
+            this.lbl_heading3.TabIndex = 2;
+            this.lbl_heading3.Text = "is";
+            this.lbl_heading3.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_name.Font = new System.Drawing.Font("Mongolian Baiti", 100F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_name.ForeColor = System.Drawing.Color.White;
+            this.lbl_name.Location = new System.Drawing.Point(70, 194);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(320, 127);
+            this.lbl_name.TabIndex = 3;
+            this.lbl_name.Text = "John";
+            this.lbl_name.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Mongolian Baiti", 100F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(245, 321);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(392, 127);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ansel";
+            this.label1.Click += new System.EventHandler(this.label1_Click_2);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Mongolian Baiti", 100F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(468, 461);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(399, 127);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Doton";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(938, 881);
+            this.ControlBox = false;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lbl_heading;
+        private System.Windows.Forms.Label lbl_heading3;
+        private System.Windows.Forms.Label lbl_heading2;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- Form3.cs
+++ Form3.cs
@@ -16,5 +16,35 @@ namespace DigitalCV
         {
             InitializeComponent();
         }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
     }
 }
